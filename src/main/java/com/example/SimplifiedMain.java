4602820410055429d352900d10f603e76e61fe4b
Rory Preddy <rorypreddy@gmail.com>
2024-06-27 12:35:09 +0200

    Add SimplifiedMain example

---
+++ src/main/java/com/example/SimplifiedMain.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class SimplifiedMain {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Uses implicit import of print and println
+    }
+}
